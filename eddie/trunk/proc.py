26c971350148af721e03b4889c4fa192cda0fd20
cmiles <cmiles@00129812-9848-11de-bf47-b33d4a03a5a8>
1997-12-19 00:10:22 +0000

    Fixed a problem with proc.py where a <defunct> process has no process name, so there wasn't enough fields after the split() and problems were occurring.... Fix was to rearrange ps output order, and if last field was missing, to call it '<defunct>'.

--- eddie/trunk/proc.py
+++ eddie/trunk/proc.py
@@ -24,7 +24,7 @@ class procList:
 	self.list = []		# list of processes
 	self.nameHash = {}	# dict of processes keyed by process name
 	 
-	rawList = os.popen(' ps -e -o "pid user comm time pcpu s" ', 'r')
+	rawList = os.popen(' ps -e -o "pid user time pcpu s comm " ', 'r')
 	rawList.readline()
  
 	for line in rawList.readlines():
@@ -61,15 +61,23 @@ class proc:
     def __init__(self, *arg):
 	self.raw = arg[0]
 
-	path = string.split(self.raw[2], "/")
-	comm = path[ len(path) - 1 ]
+	print 'self.raw:',self.raw
+
+	try:
+	    path = string.split(self.raw[5], "/")
+	    comm = path[ len(path) - 1 ]
+	except IndexError:
+	    # This process has no command associated with it.  This can happen
+	    # when a process is <defunct>.
+	    # For now, let's call it "<defunct>"...
+	    comm = '<defunct'
 
 	self.pid = self.raw[0]		# pid
 	self.user = self.raw[1]		# user
+	self.time = self.raw[2]		# cpu time
+	self.percent = self.raw[3]	# percentage of cpu
+	self.status = self.raw[4]	# status
 	self.command = comm		# command	
-	self.time = self.raw[3]		# cpu time
-	self.percent = self.raw[4]	# percentage of cpu
-	self.status = self.raw[5]	# status
 
 
     def __str__(self):
