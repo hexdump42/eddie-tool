6edb35c41e0993799f5bd0e54f4bbaed79f8d8d7
cmiles <cmiles@00129812-9848-11de-bf47-b33d4a03a5a8>
1998-04-21 05:30:38 +0000

    bin now only contains the main "executable". (still have to change import path so don't checkout yet...)

---
+++ eddie/trunk/bin/eddie.py
@@ -0,0 +1,200 @@
+#!/opt/local/bin/python
+## 
+## File         : eddie.py 
+## 
+## Author       : Rod Telford  <rtelford@connect.com.au>
+##                Chris Miles  <cmiles@connect.com.au>
+## 
+## Date         : 971204 
+## 
+## Description  : Eddie main program
+##
+## $Id$
+##
+
+
+import os
+import time
+import signal
+import sys
+
+import proc
+import df
+import netstat
+import parseConfig
+import directive
+import definition
+import config
+import action
+import log
+import history
+
+# Main config file - this file INCLUDEs all other config files
+config_file = 'config/eddie.cf'
+
+
+# Exit Eddie cleanly
+def eddieexit():
+    # email admin any remaining messages
+    log.sendadminlog(1)
+    sys.exit()
+
+
+# Signal Handler
+def SigHandler( sig, frame ):
+    global ourList		# global list of all directives
+    global defDict		# global dictionary of DEFinitions
+    global MDict		# global dictionary of Messages
+    global ADict		# global dictionary of Actions
+
+    if sig == signal.SIGHUP:
+	# SIGHUP (Hangup) - reload config
+	log.log( '<eddie>SigHandler(), SIGHUP encountered - reloading config', 2 )
+	#
+	# reset lists and read in config and rules
+	ourList = directive.Rules()
+	defDict = {}
+	MDict = definition.MsgDict()
+	ADict = {}
+	parseConfig.readFile(config_file, ourList, defDict, MDict, ADict)
+	directive.ADict = ADict		# make ADict viewable in directive module
+	action.MDict = MDict		# make MDict viewable in action module
+
+    elif sig == signal.SIGINT:
+	# SIGINT (CTRL-c) - quit now
+	log.log( '<eddie>SigHandler(), SIGINT (KeyboardInterrupt) encountered - quitting', 2 )
+	print "\nEddie quitting ... bye bye"
+	eddieexit()
+
+    elif sig == signal.SIGTERM:
+	# SIGTERM (Terminate) - quit now
+	log.log( '<eddie>SigHandler(), SIGTERM (Terminate) encountered - quitting', 2 )
+	print "\nEddie quitting ... bye bye"
+	eddieexit()
+
+    else:
+	# un-handled signal - log & ignore it
+	log.log( '<eddie>SigHandler(), unknown signal received, %d - ignoring' % sig, 2 )
+
+
+# The guts of the Eddie program - sets up the lists, reads config info, gets
+# system information, then performs the checking.
+def eddieguts(eddieHistory):
+
+    #print "M: ",ourList['M']
+    #print "D: ",ourList['D']
+    #print "FS: ",ourList['FS']
+    #print "PID: ",ourList['PID']
+    #print "SP: ",ourList['SP']
+
+
+    # instantiate a process list
+    log.log( "<eddie>eddieguts(), creating process list", 8 )
+    directive.plist = proc.procList()
+
+    # instantiate a disk usage list
+    log.log( "<eddie>eddieguts(), creating df list", 8 )
+    directive.dlist = df.dfList()
+
+    # instantiate a netstat list
+    log.log( "<eddie>eddieguts(), creating netstat list", 8 )
+    directive.nlist = netstat.netstatList()
+
+    ## debugging ##
+    #print "-- The following DEFs are defined: --"
+    #for i in defDict.keys():
+    #	print "%s=%s" % (i, defDict[i])
+    #print "-- The following Ms are defined: --"
+    #for i in MDict.keys():
+    #	print "%s: Subject \"%s\"\n---BODY---\n%s\n----------" % (i, MDict.subj(i), MDict[i])
+    #print "-- The following As are defined: --"
+    #for i in ADict.keys():
+    #	print "%s=%s" % (i, ADict[i])
+
+    # Now do all the checking
+    # note ... directive order is not defined (we don't currently care do we?)
+    log.log( "<eddie>eddieguts(), beginning checks", 8 )
+
+    ## debugging - test with 'D' directive for now ##
+    for d in ourList.keylist():
+    #for d in ('COM',):
+	list = ourList[d]
+	if list != None:
+	    for i in list:
+		i.docheck()
+	else:
+	    log.log( "<eddie>eddieguts(), ourList['%s'] is empty" % (d), 6 )
+
+    # Save history (debug.. FS only for now...)
+    eddieHistory.save('FS',directive.dlist)
+
+
+
+def main():
+    global ourList		# global list of all directives
+    global defDict		# global dictionary of DEFinitions
+    global MDict		# global dictionary of Messages
+    global ADict		# global dictionary of Actions
+
+    signal.signal( signal.SIGALRM, signal.SIG_IGN )
+    signal.signal( signal.SIGHUP, SigHandler )
+    signal.signal( signal.SIGINT, SigHandler )
+    signal.signal( signal.SIGTERM, SigHandler )
+
+    #    TODO: Is there a simpler Python-way of getting hostname ??
+    #    TODO: Get the hostname at program startup, not here...
+    tmp = os.popen('uname -n', 'r')
+    hostname = tmp.readline()
+    log.hostname = hostname[:-1]	# strip \n off end
+    tmp.close()
+
+    # New history object
+    history.eddieHistory = history.history()
+
+    # initialise our global lists/dicts
+    ourList = directive.Rules()
+    defDict = {}
+    MDict = definition.MsgDict()
+    ADict = {}
+
+    # read in config and rules
+    parseConfig.readFile(config_file, ourList, defDict, MDict, ADict)
+
+    directive.ADict = ADict		# make ADict viewable in directive module
+    action.MDict = MDict		# make MDict viewable in action module
+
+    # Main Loop
+    while 1:
+	try:
+	    # perform guts of Eddie
+	    eddieguts(history.eddieHistory)
+
+	    # email admin the adminlog if required
+	    log.sendadminlog()
+
+	    # sleep for set period - only quits with CTRL-c
+	    log.log( '<eddie>main(), sleeping for %d secs' % (config.scanperiod), 6 )
+	    #print 'Press CTRL-C to quit'
+
+	    # Sleep by setting SIGALRM to go off in scanperiod seconds
+	    #time.sleep( config.scanperiod )
+	    signal.alarm( config.scanperiod )
+	    signal.pause()
+
+	except KeyboardInterrupt:
+	    # CTRL-c hit - quit now
+	    log.log( '<eddie>main(), KeyboardInterrupt encountered - quitting', 2 )
+	    print "\nEddie quitting ... bye bye"
+	    break
+
+
+
+if __name__ == "__main__":
+    main()
+
+    # email admin anything else...
+    log.sendadminlog()
+
+###
+### END eddie.py
+###
