66f5ce8ef232d7ef0ff8566c9013a2ce4ae27af8
nobody <nobody@00129812-9848-11de-bf47-b33d4a03a5a8>
1998-05-15 00:29:58 +0000

    This commit was manufactured by cvs2svn to create tag 'REL0_10'.

---
+++ eddie/tags/REL0_10/lib/common/definition.py
@@ -0,0 +1,161 @@
+#!/opt/local/bin/python 
+## 
+## File		: definition.py 
+## 
+## Author       : Rod Telford  <rtelford@connect.com.au>
+##                Chris Miles  <cmiles@connect.com.au>
+## 
+## Date		: 971215
+## 
+## Description	: 
+##
+## $Id$
+##
+##
+
+import string
+import regex
+import utils
+import log
+
+# Define exceptions
+ParseFailure = 'ParseFailure'
+
+
+##
+## The Message class that acts as a dictionary...
+##
+class MsgDict:
+    def __init__(self):
+	self.hash = {}		# Dictionary of M objects keyed by name
+
+    # Overload '+', eg: rules + directive_rule
+    def __add__(self, new):
+	self.hash[new.name] = new	# Add M object to dictionary
+	return(self)
+
+    # Overload '[]', eg: returns corresponding message for M-name
+    def __getitem__(self, name):
+	try:
+	    return self.hash[name].message
+	except KeyError:
+	    return None
+
+    def subj(self, name):
+	try:
+	    return self.hash[name].subject
+	except KeyError:
+	    return None
+
+    def keys(self):
+	return self.hash.keys()
+    
+    def delete(self, name):
+	del self.hash[name]
+
+
+##
+## The base definition class.  Derive all definition-types from this base class.
+##
+class Definition:
+    def __init__(self, *arg):
+	self.raw = arg[0]			# the raw line as read from config file
+	self.basetype = 'Definition'		# the object can know its own basetype
+	self.type = string.split(self.raw)[0]	# the definition type of this instance
+	self.regexp = ''			# the regexp to split the raw line into fields
+
+    def getRaw(self):
+	return self.raw
+
+    def parseRaw(self):
+	sre = regex.compile( self.regexp )
+	inx = sre.search( self.raw )
+	if inx == -1:
+	    # probably make an exception here.....
+	    raise ParseFailure, "Error while parsing line: "+self.raw
+	fieldlist = ()
+	i=1
+	while( sre.group(i) != None ):
+	    fieldlist = fieldlist + (sre.group(i),)
+	    i = i + 1
+	return fieldlist
+
+## -- DEFINITIONS --
+
+##
+## MESSAGE DEFINITION
+##
+class M(Definition):
+    def __init__(self, *arg):
+	apply( Definition.__init__, (self,) + arg )
+	self.regexp = 'M[\t ]+\([a-zA-Z0-9_/\.]+\)[\t ]+\"\(.*\)\"[\t \n]+\([^\005]*\)'
+	fields = self.parseRaw()
+	self.name = fields[0]
+	self.subject = fields[1]
+	self.message = fields[2]
+	log.log( "<Definition>M(), M created, name '%s', subject '%s', message '%s'" % (self.name,self.subject,self.message), 9 )
+
+
+##
+## DEF DEFINITION - defines general definitions, access with $defn_name
+##
+class DEF(Definition):
+    def __init__(self, *arg):
+	apply( Definition.__init__, (self,) + arg )
+	self.regexp = 'DEF[\t ]+\([a-zA-Z0-9_]+\)[\t ]*=[\t ]*\(.*\)[\t \n]*$'
+	fields = self.parseRaw()
+	self.name = fields[0]		# the name of this DEFinition
+	self.text = fields[1]		# the text that is assigned to it
+	log.log( "<Definition>DEF(), DEF created, name '%s', text '%s'" % (self.name,self.text), 8 )
+
+
+##
+## A DEFINITION - defines Actions
+class A(Definition):
+    def __init__(self, *arg):
+	apply( Definition.__init__, (self,) + arg )
+	self.regexp = 'A[\t ]+\([a-zA-Z0-9_]+\)[\t ]+\(.*\)'
+	fields = self.parseRaw()
+	self.name = fields[0]		# the name of this Action
+	self.text = fields[1]		# the text that is assigned to it
+	log.log( "<Definition>A(), A created, name '%s', text '%s'" % (self.name,self.text), 8 )
+
+
+##
+## parseList( list, dict ) - cycles thru list, if any entries are keys in dict, replaces
+##   entry with value from dict.
+def parseList( list, dict ):
+    newlist = []
+    for l in list:
+	try:
+	    # is l a key in dict ?  if yes, it is added
+	    tmp = dict[l]
+	    tmplist = utils.trickySplit( tmp, ',' )
+	    tmplist = parseList( tmplist, dict )
+	    for i in tmplist:
+		newlist.append(i)
+	except KeyError:
+	    # if no, just add l
+	    newlist.append(l)
+    return newlist
+
+
+##
+## parseM( text, dict ) - if text is in dict (assumed to be of type MsgDict)
+##   then (subj,body) list is returned, else empty list is returned.
+def parseM( text, dict ):
+    try:
+	# is text a key in dict ?
+	body = dict[text]
+    except KeyError:
+	# if no, return empty list
+	return ()
+    if body == None:
+	# not in dictionary, return empty list
+	return ()
+    return (dict.subj(text), body)
+
+
+##
+## END - definition.py
+##
---
+++ eddie/tags/REL0_10/lib/common/utils.py
@@ -0,0 +1,155 @@
+#!/opt/local/bin/python 
+## 
+## File		: utils.py 
+## 
+## Author       : Rod Telford  <rtelford@connect.com.au>
+##                Chris Miles  <cmiles@connect.com.au>
+## 
+## Date		: 971217 
+## 
+## Description	: General utility functions
+##
+## $Id$
+##
+##
+
+import regex
+import string
+
+##
+## trickySplit( line, delim ) - split line by delimiter delim, but ignoring
+## delimiters found inside ()'s, []'s, {}'s, '''s and ""'s.
+##
+## eg: trickySplit( "email(root,'hi there'),system('echo hi, mum')", ',' )
+## would return: [ "email(root,'hi there')", "system('echo hi, mum')" ]
+##
+def trickySplit( line, delim ):
+    parenCnt = 0		# count of ()'s
+    curlyCnt = 0		# count of {}'s
+    squareCnt = 0		# count of []'s
+    doubleqCnt = 0		# count of ""'s
+    quoteCnt = 0		# count of '''s
+
+    list = []			# list of split strings
+    current = ''		# the current split string
+
+    for c in line:
+	if c == '(':
+	    parenCnt = parenCnt + 1
+	elif c == ')':
+	    parenCnt = parenCnt - 1
+	elif c == '{':
+	    curlyCnt = curlyCnt + 1
+	elif c == '}':
+	    curlyCnt = curlyCnt - 1
+	elif c == '[':
+	    squareCnt = squareCnt + 1
+	elif c == ']':
+	    squareCnt = squareCnt - 1
+	elif c == '"':
+	    doubleqCnt = 1 - doubleqCnt
+	elif c == "'":
+	    quoteCnt = 1 - quoteCnt
+	elif c == delim:
+	    if parenCnt == 0 and curlyCnt == 0 and squareCnt == 0 and doubleqCnt == 0 and quoteCnt == 0:
+		# split here!
+		list.append(current)
+		current = ''
+		continue
+
+	current = current + c
+
+    if len(current) > 0:
+	list.append(current)
+
+    return list
+
+
+##
+## quoteArgs( list ) - cycle through list of strings, if the string looks like a
+##   function call (eg: "blah( a, b, c )") then put quotes around each of the
+##   arguments.  [Useful if you want to pass the string to eval()].  eg: the
+##   previous example would be converted to 'blah( "a", "b", "c" )'.
+##
+def quoteArgs( list ):
+    newlist = []
+    sre = regex.compile( "\([\t ]*[A-Za-z0-9_]*[\t ]*(\)\(.*\)\([\t ]*)[\t ]*\)" )
+    for s in list:
+	inx = sre.search( s )
+	if inx != -1:
+	    argline = sre.group(2)
+	    arglist = string.split( argline, ',' )
+	    newcmd = sre.group(1)		# build new command
+	    i = 0				# count arguments so we don't put ',' before 1st argument
+	    for a in arglist:
+		a = string.strip( a )
+		# if argument is not already enclosed in quotes ("" or '')
+		if regex.search( "^[\"'].*[\"']$", a ) == -1:
+		    a = '"' + a + '"'		# enclose it in quotes
+		if i > 0:
+		    newcmd = newcmd + ','	# put comma before argument 2-onwards
+		newcmd = newcmd + a
+		i = i + 1
+	    newcmd = newcmd + sre.group(3)
+	    newlist.append( newcmd )		# add our "quote-arg'd" command to list
+	else:
+	    newlist.append( s )			# add un-changed command to list
+    
+    return newlist
+	
+ 
+
+##
+## charpresent( s, chars ) - returns 1 if ANY of the characters present in the string
+##   chars is found in the string s.  If none are found, 0 is returned.
+##
+def charpresent( s, chars ):
+    for c in chars:
+	if string.find( s, c ) != -1:
+	    return 1
+    return 0
+
+
+##
+## stripquote( s ) - strips start & end of string s of whitespace then
+##   strips " or ' from start & end of string if found
+##
+def stripquote( s ):
+    s = string.strip( s )
+
+    if s[0] == "'" or s[0] == '"':
+	s = s[1:]
+    if s[-1] == "'" or s[-1] == '"':
+	s = s[:-1]
+
+    return s
+
+
+##
+## atom( ch ) - ascii-to-multiplyer - converts ascii char to a time multiplyer.
+##   eg: s=seconds, m=minutes, h=hours, d=days, w=weeks, c=calendar=months, y=years
+##
+def atom( ch ):
+    if ch == 's' or ch == 'S':
+	mult = 1
+    elif ch == 'm' or ch == 'M':
+	mult = 60
+    elif ch == 'h' or ch == 'H':
+	mult = 60*60
+    elif ch == 'd' or ch == 'D':
+	mult = 60*60*24
+    elif ch == 'w' or ch == 'W':
+	mult = 60*60*24*7
+    elif ch == 'c' or ch == 'C':
+	mult = 60*60*24*30			# not exact...
+    elif ch == 'y' or ch == 'Y':
+	mult = 60*60*24*365
+    else:
+	return 0
+
+    return mult
+	
+
+##
+## END - utils.py
+##
