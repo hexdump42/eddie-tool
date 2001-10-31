dbd18abb3b7486c00964cbd1506a90b2bd96974e
chris <chris@00129812-9848-11de-bf47-b33d4a03a5a8>
2001-10-31 07:02:30 +0000

    Added full command-line argument support. Cleaned up logging - can define different logging levels (minimal, verbose, debug) and optionally log to a logfile. Version is now 2.1.
    Example:
    elvinrrd.py -v -c elvinrrd.cf -l /var/log/elvinrrd.log -e elvin://elvin.domain.name/

--- elvinrrd/trunk/elvinrrd/elvinrrd.py
+++ elvinrrd/trunk/elvinrrd/elvinrrd.py
@@ -1,26 +1,24 @@
 #!/opt/python2/bin/python
 ## 
 ## File         : elvinrrd.py
-## 
-## Author       : Chris Miles  <cmiles@connect.com.au>
-## 
-## Date         : 20010827
-## 
-## Description  : Elvin4 consumer to insert Eddie STORE data into an RRDtool database.
+## Author       : Chris Miles <chris@psychofx.com>
+## Start Date   : 20010827
+## Description  : Elvin4 consumer to insert data sent via Elvin into RRDtool databases.
 ##
 ## $Id$
 ##
 
 ################################################################
 
-__version__ = """2.0"""
+__version__ = """2.1"""
 
 ################################################################
 
-import sys, traceback, re, string, os
+import sys, traceback, re, string, os, getopt, time
 import RRDtool	# requires PyRRDtool from http://cvsweb.extreme-ware.com/cvsweb.cgi/PyRRDtool/
 import elvin	# requires Elvin4 modules from http://elvin.dstc.edu.au/
 
+# Default Elvin URL and SCOPE
 ELVIN_URL='elvin://elvin'
 ELVIN_SCOPE='elvin'
 
@@ -59,13 +57,17 @@ class eddieElvin:
 	else:
 	    connect_string='*'		# auto discovery
 
-	print "Trying Elvin connection to %s" % (connect_string)
+	if verbose:
+	    log( "Trying Elvin connection to %s" % (connect_string) )
 
 	try:
 	    self.elvinc = elvin.connect( connect_string )
-	    print "Elvin connection succeeded."
+	    if verbose:
+		log( "Elvin connection succeeded to %s" % (connect_string) )
 	except:
-	    sys.stderr.write("Connection to elvin failed - connection string was '%s'\n" % (connect_string) )
+	    sys.stderr.write( "Connection to elvin failed - connection string was '%s'\n" % (connect_string) )
+	    if logfile != None:
+		log( "Connection to elvin failed - connection string was '%s'\n" % (connect_string) )
 	    sys.exit(1)
 
 
@@ -104,7 +106,7 @@ class storeconsumer(eddieElvin):
 		    break
 
 	if r == None:
-	    print "No match for message %s" % (msg)
+	    log( "warning: No match for message %s" % (msg) )
 	    return 1
 
 	rrdfile = r.rrdfile
@@ -123,11 +125,12 @@ class storeconsumer(eddieElvin):
 	try:
 	    val = msg[u'%s'%(store)]
 	except KeyError, err:
-	    print "KeyError, %s, message %s" % (err, msg)
+	    log( "KeyError: %s, message %s" % (err, msg) )
 	    return 1
 
 	u = (rrdfile, "N:%s" % (str(val)))
-	print ' rrd.update( %s )' % (u,)
+	if debug:
+	    log( 'rrd.update( %s )' % (u,) )
 
 	try:
 	    self.rrd.update( u )
@@ -135,20 +138,25 @@ class storeconsumer(eddieElvin):
 	    if str(err).find('No such file or directory') != -1:
 		if os.path.exists( rrdfile ):
 		    # file exists, despite the error...
-		    print "IOError, %s, message %s" % (err, msg)
+		    sys.stderr.write( "IOError: %s, message %s\n" % (err, msg) )
+		    if logfile:
+			log( "IOError: %s, message %s" % (err, msg) )
 		else:
-		    print "creating",rrdfile
                     rrd_dir = os.path.dirname( rrdfile )
 
                     if not os.path.exists( rrd_dir ):
-                        print "creating directory", rrd_dir
+			if verbose:
+			    log( "Creating directory '%s'" % (rrd_dir) )
                         os.makedirs( rrd_dir )
 
 		    createargs = (rrdfile,) + tuple(create.split())
-		    print "creating rrd:",createargs
-		    rrd.create( createargs )
+		    if verbose:
+			log( "Creating rrd: %s" % (createargs,) )
+		    self.rrd.create( createargs )
 	    else:
-	        print "IOError, %s, message %s" % (err, msg)
+	        sys.stderr.write( "IOError: %s, message %s\n" % (err, msg) )
+		if logfile:
+		    log( "IOError: %s, message %s\n" % (err, msg) )
 
 	return 0
 
@@ -156,12 +164,18 @@ class storeconsumer(eddieElvin):
 def ReadConfig( filename ):
     """Read the configuration from the given filename."""
 
+    if verbose:
+	log( "Reading config from '%s'" % (filename) )
+
     rrddict = {}
 
     try:
         fp = open(filename, 'r')
     except IOError:
-        print "Cannot open configuration file '%s', exiting." % (filename)
+        sys.stderr.write( "Cannot open configuration file '%s', exiting" % (filename) )
+	if logfile:
+	    log( "Cannot open configuration file '%s', exiting" % (filename) )
+	sys.exit(1)
 
     re_comment = "^\s*#.*$"
     re_empty = "^\s*$"
@@ -206,7 +220,9 @@ def ReadConfig( filename ):
 		elif inx.group(1) == 'create':
 		    create = inx.group(2)
 		else:
-		    print "Parse error, unknown keyword '%s' on following line:\n%s" % (inx.group(1),line)
+		    sys.stderr.write( "Parse error, unknown keyword '%s' on following line:\n%s" % (inx.group(1),line) )
+		    if logfile:
+			log( "Parse error, unknown keyword '%s' on following line:\n%s" % (inx.group(1),line) )
 		    sys.exit(1)
 
         line = fp.readline()
@@ -218,26 +234,102 @@ def ReadConfig( filename ):
 
     fp.close()
 
+    if verbose:
+	log( "%s parsed, %d entries in config" % (filename,len(rrddict.keys())) )
+
     return rrddict
 
 
-if __name__ == "__main__":
+def log( text ):
+    """Log text to either logfile (if defined) or stdout."""
+
+    if logfile != None:
+	try:
+	    fp = open(logfile, 'a')
+	except IOError, err:
+	    sys.stderr.write( "error: IOError opening '%s', %s\n" % (logfile, err) )
+	    sys.exit(1)
+	t = "%04d-%02d-%02d %02d:%02d:%02d" % (time.localtime()[0:6])
+	fp.write( "%s %s\n" % (t, text) )
+	fp.close()
+
+    else:
+	sys.stdout.write( "%s\n" % (text) )
+
+
+def main():
+    usage_short = "[-dhv] -c elvinrrd.cf [-e elvin_url] [-s elvin_scope] [-l logfile]"
+    usage_long = "[--debug] [--help] [--verbose] --configfile elvinrrd.cf [--elvinurl elvin_url] [--elvinscope elvin_scope] [--logfile logfile]"
+    usage = "usage: %s %s\n   or: %s" % (sys.argv[0], usage_short, usage_long)
+
+    if len(sys.argv) <= 1:
+	print usage
+	sys.exit(1)
+
+    # Configurable settings
+    global debug
+    debug = 0
+    global verbose
+    verbose = 0
+    configfile = None
+    global logfile
+    logfile = None
+    elvin_url = ELVIN_URL
+    elvin_scope = ELVIN_SCOPE
+
+    # Handle command-line arguments
+    options = "dhvc:e:s:l:"
+    long_options = ["debug", "help", "verbose", "configfile=", "elvin_url=", "elvin_scope=", "logfile="]
+    try:
+	opts, args = getopt.getopt(sys.argv[1:], options, long_options)
+    except getopt.GetoptError:
+	print usage
+	sys.exit(1)
+    for o, a in opts:
+	if o in ("-h", "--help"):
+	    print usage
+	    sys.exit(1)
+	if o in ("-v", "--verbose"):
+	    verbose = 1
+	if o in ("-d", "--debug"):
+	    debug = 1
+	    verbose = 1
+	if o in ("-c", "--configfile"):
+	    configfile = a
+	if o in ("-e", "--elvinurl"):
+	    elvin_url = a
+	if o in ("-s", "--elvinscope"):
+	    elvin_scope = a
+	if o in ("-l", "--logfile"):
+	    logfile = a
+
+    if verbose:
+	log( "elvinrrd version %s starting" % (__version__) )
 
     # Build config
-    rrddict = ReadConfig( sys.argv[1] )
+    if configfile == None:
+	sys.stderr.write( "error: No configfile defined\n" )
+	sys.exit(1)
+
+    rrddict = ReadConfig( configfile )
     if rrddict == None or rrddict == {}:
-	print "Error, no configuration."
+	sys.stderr.write( "error: configuration is empty\n" )
 	sys.exit(1)
 
     # Create RRDtool object
     rrd = RRDtool.RRDtool()
 
-    e = storeconsumer()
+    e = storeconsumer(elvin_url, elvin_scope)
     e.rrd = rrd
     e.rrddict = rrddict
     e.register()
+    if verbose:
+	log( "Starting Elvin main loop" )
     e.elvinc.run()
 
+
+if __name__ == "__main__":
+    main()
 
 
 ###
