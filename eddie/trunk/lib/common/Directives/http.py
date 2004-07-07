4c64ba609b34ba57357f7f1ff44e6abae351de1a
chris <chris@00129812-9848-11de-bf47-b33d4a03a5a8>
2004-07-07 22:21:18 +0000

    Enhancements to HTTP directive:
    Supports URLs with non-standard ports, eg: http://localhost:8080/
    
    Added finer grained timing of four parts of the HTTP connection:
      time_resolve  - elapsed time to resolve hostname to IP
      time_connect  - elapsed time to connect to server
      time_request  - elapsed time to send HTTP/S request to server
      time_response - elapsed time to retrieve the server response (and close connection)
      time          - elapsed total time (sum of above)
    
    Added better logging of HTTP directive actions.

--- eddie/trunk/lib/common/Directives/http.py
+++ eddie/trunk/lib/common/Directives/http.py
@@ -27,9 +27,15 @@
 
 
 # Imports: Python
-import httplib, re, time, sys, string
+import httplib
+import re
+import time
+import sys
+import string
+import socket
 # Imports: Eddie
-import directive, log
+import directive
+import log
 
 
 ##
@@ -38,8 +44,7 @@ import directive, log
 
 
 class HTTP(directive.Directive):
-    """
-    Uses httplib module to make a HTTP (or HTTPS) connection and request
+    """Uses httplib module to make a HTTP (or HTTPS) connection and request
     an object.  The elapsed connection time is recorded, and all related
     connection variables are made available, such as response code and
     returned message body, as well as error information if the connection
@@ -47,7 +52,7 @@ class HTTP(directive.Directive):
 
     SSL-support must be compiled into Python for HTTPS connections.
 
-    The POST method is not yet supported.
+    The POST method is not yet supported. (TODO)
     """
 
     def __init__(self, toklist):
@@ -55,8 +60,7 @@ class HTTP(directive.Directive):
 
 
     def tokenparser(self, toklist, toktypes, indent):
-	"""
-	Parse directive arguments.
+	"""Parse directive arguments.
 	"""
 
 	apply( directive.Directive.tokenparser, (self, toklist, toktypes, indent) )
@@ -78,12 +82,25 @@ class HTTP(directive.Directive):
 	    self.ssl = 0
 
 	# Parse URL
-	url_re = "https?://(?P<hostname>[a-zA-Z0-9.-]+)(?P<file>/.*)?"
+	url_re = "https?://(?P<hostname>[a-zA-Z0-9.-]+)(:(?P<port>[0-9]+))?(?P<file>/.*)?"
 	inx = re.match(url_re, self.args.url)
 	if inx == None:
 	    log.log( "<http>HTTP.tokenparser(): not a valid URL '%s'" % (self.args.url), 1 )
 	    raise directive.ParseFailure, "not a valid URL '%s'" % (self.args.url)
+
 	self.hostname = inx.group('hostname')
+	self.port = inx.group('port')
+	if self.port == None:
+	    if self.ssl:
+		self.port = 443
+	    else:
+		self.port = 80
+	else:
+	    try:
+		self.port = int(self.port)
+	    except ValueError, msg:
+		raise directive.ParseFailure, "%s: port invalid for URL %s, %s" %(self.ID,self.args.url,msg)
+
 	self.file = inx.group('file')
 	if self.file == None:
 	    self.file = '/'
@@ -92,6 +109,7 @@ class HTTP(directive.Directive):
 	self.defaultVarDict['url'] = self.args.url
 	self.defaultVarDict['rule'] = self.args.rule
 	self.defaultVarDict['hostname'] = self.hostname
+	self.defaultVarDict['port'] = self.port
 	self.defaultVarDict['file'] = self.file
 
 	# define the unique ID
@@ -103,13 +121,16 @@ class HTTP(directive.Directive):
 
 
     def getData(self):
-	"""
-	Called by Directive docheck() method to fetch the data required for
+	"""Called by Directive docheck() method to fetch the data required for
 	evaluating the directive rule.
 
 	Data:
 	    failed	(boolean) - true if connection failed (socket error usually)
-	    time	(float)   - elapsed time of connection
+	    time	  (float) - total elapsed time of connection
+	    time_resolve  (float) - elapsed time to resolve hostname  to IP
+	    time_connect  (float) - elapsed time to connect to server
+	    time_request  (float) - elapsed time to send HTTP/S request to server
+	    time_response (float) - elapsed time to retrieve the server response (and close connection)
 
 	    if failed:
 		exception (string) - exception type
@@ -126,6 +147,9 @@ class HTTP(directive.Directive):
 		body	(string)  - contains HTTP message body
 	"""
 
+	# cmiles 2004-07-05: modified to record elapsed time of each section of the HTTP(S)
+	#	connection: IP resolution, connection, sending request, receiving response
+
 	# Initialize the data
 	data = {}
 	data['failed'] = 0
@@ -133,59 +157,141 @@ class HTTP(directive.Directive):
 	data['errno'] = ""
 	data['errstr'] = ""
 	data['time'] = ""
+	data['time_resolve'] = ""
+	data['time_connect'] = ""
+	data['time_request'] = ""
+	data['time_response'] = ""
 
+	e = None
+
+	# Resolve the hostname to IP address (and time it)
+	log.log( "<http>HTTP.getData(): Resolving %s"%(self.hostname), 7 )
+	time_resolve_start = time.time()
+	try:
+	    ip = socket.gethostbyname( self.hostname )
+	except:
+	    time_resolve_end = time.time()
+	    e = sys.exc_info()
+	    data['exception'] = e[0]
+	    data['errno'] = e[1][0]
+	    data['errstr'] = e[1][1]
+	    data['time_resolve'] = time_resolve_end - time_resolve_start
+	    data['time'] = data['time_resolve']
+	    data['failed'] = 1
+	    log.log( "<http>HTTP.getData(): Resolving %s failed, exception=%s, errno=%s, errstr=%s"%(self.hostname,data['exception'],data['errno'],data['errstr']), 7 )
+	    return data
+
+	time_resolve_end = time.time()
+	data['time_resolve'] = time_resolve_end - time_resolve_start
+	data['ip'] = ip
+	log.log( "<http>HTTP.getData(): Resolved %s to: %s, time=%f"%(self.hostname,ip,data['time_resolve']), 7 )
+
+	time_start = time_resolve_start
+	time_end = time_resolve_end
+
+	# Initialize httplib - use IP address to avoid any more DNS lookups
 	if self.ssl:
 	    try:
-		conn = httplib.HTTPSConnection( self.hostname )
+		conn = httplib.HTTPSConnection( ip, self.port )
 	    except AttributeError:
 		log.log( "<http>HTTP.getData(): cannot open HTTPS connection, SSL module not available", 3 )
 		raise directive.DirectiveError, "cannot open HTTPS connection, SSL module not available"
 	else:
-	    conn = httplib.HTTPConnection( self.hostname )
+	    conn = httplib.HTTPConnection( ip, self.port )
 
-	extra_headers = { 'User-Agent': 'EDDIE-Tool/%s'%(log.version) }
+	# Uncomment this to send httplib debug output to stdout
+	#conn.set_debuglevel(1)
 
-	time_start = time.time()
+	# Create the HTTP/HTTPS connection (and time it)
+	log.log( "<http>HTTP.getData(): Connecting to: %s:%d (%s)"%(ip,self.port,self.hostname), 7 )
+	time_connect_start = time.time()
+	try:
+	    conn.connect()
+	except:
+	    time_connect_end = time.time()
+	    e = sys.exc_info()
+	    data['exception'] = e[0]
+	    data['errno'] = e[1][0]
+	    data['errstr'] = e[1][1]
+	    data['time_connect'] = time_connect_end - time_connect_start
+	    data['time'] = time_connect_end - time_start
+	    data['failed'] = 1
+	    log.log( "<http>HTTP.getData(): Connection to %s failed, exception=%s, errno=%s, errstr=%s"%(self.hostname,data['exception'],data['errno'],data['errstr']), 7 )
+	    return data
+
+	time_connect_end = time.time()
+	data['time_connect'] = time_connect_end - time_connect_start
+	log.log( "<http>HTTP.getData(): Connected to: %s, time=%f"%(self.hostname,data['time_connect']), 7 )
+
+	# Send user-agent to be a good netizen
+	# Set hostname so HTTP/1.1 works properly (we connect to the IP address above)
+	extra_headers = { 'User-Agent': 'EDDIE-Tool/%s'%(log.version),
+			       'Host' : self.hostname 
+			}
+
+	# Send request to server
+	log.log( "<http>HTTP.getData(): Sending request: %s"%(self.file), 7 )
+	time_request_start = time.time()
 	try:
 	    conn.request( "GET", self.file, None, extra_headers )
 	except:
-	    time_end = time.time()
+	    time_request_end = time.time()
 	    e = sys.exc_info()
 	    data['exception'] = e[0]
 	    data['errno'] = e[1][0]
 	    data['errstr'] = e[1][1]
-	    data['time'] = time_end - time_start
-	    data['failed'] = 1
+	    data['time_request'] = time_request_end - time_request_start
+	    data['time'] = time_request_end - time_start
+	    data['failed'] = 2
+	    log.log( "<http>HTTP.getData(): Request failed, exception=%s, errno=%s, errstr=%s"%(data['exception'],data['errno'],data['errstr']), 7 )
+	    conn.close()
 	    return data
 
+	time_request_end = time.time()
+	data['time_request'] = time_request_end - time_request_start
+	log.log( "<http>HTTP.getData(): Request sent, time=%f"%(data['time_request']), 7 )
+
+	time_end = time_request_end
+
+	# Get response from server
+	log.log( "<http>HTTP.getData(): Waiting to receive response", 7 )
+	time_response_start = time.time()
 	try:
 	    r = conn.getresponse()
 	except:
-	    time_end = time.time()
+	    time_response_end = time.time()
 	    e = sys.exc_info()
 	    data['exception'] = e[0]
 	    data['errno'] = e[1][0]
 	    data['errstr'] = e[1][1]
-	    data['time'] = time_end - time_start
-	    data['failed'] = 1
+	    data['time_response'] = time_response_end - time_response_start
+	    data['time'] = time_response_end - time_start
+	    data['failed'] = 3
+	    log.log( "<http>HTTP.getData(): Receive response failed, exception=%s, errno=%s, errstr=%s"%(data['exception'],data['errno'],data['errstr']), 7 )
 	    conn.close()
 	    return data
+
+	try:	# cmiles 2003-04-02: httplib can fail when reading body sometimes
+	    data['body'] = r.read()
+	except:
+	    e = sys.exc_info()
+	    log.log( "<http>HTTP.getData(): response read failed, exception=%s, errno=%s, errstr=%s"%(e[0],e[1][0],e[1][1]), 7 )
+	    data['body'] = "<read failed>"
 
 	data['status'] = r.status
 	data['reason'] = r.reason
 	data['length'] = r.length
 	data['version'] = r.version
 	data['header'] = str(r.msg)
-	try:	# cmiles 2003-04-02: httplib can fail when reading body sometimes
-	    data['body'] = r.read()
-	except:
-	    data['body'] = ""
 
 	conn.close()
 
 	# cmiles - 2003-04-02: moved time_end from before getresponse() to after close()
-	time_end = time.time()
-	data['time'] = time_end - time_start
+	time_response_end = time.time()
+	data['time_response'] = time_response_end - time_response_start
+	data['time'] = time_response_end - time_start
+
+	log.log( "<http>HTTP.getData(): Response received, status=%s, reason=%s, time=%f, total elapsed time=%f"%(data['status'],data['reason'],data['time_response'],data['time']), 7 )
 
 	# Set an ok if status is 2xx or 3xx
 	if r.status >= 200 and r.status < 400:
