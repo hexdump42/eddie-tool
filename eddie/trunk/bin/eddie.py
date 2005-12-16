43fcc2172c2dcc822d74952a431faf4ea2d108d4
chris <chris@00129812-9848-11de-bf47-b33d4a03a5a8>
2005-12-16 18:05:27 +0000

    __version__='0.36-svn'

--- eddie/trunk/bin/eddie.py
+++ eddie/trunk/bin/eddie.py
@@ -26,7 +26,7 @@
 ## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 ########################################################################
 
-__version__='0.35'
+__version__='0.36-svn'
 
 
 # Python modules
