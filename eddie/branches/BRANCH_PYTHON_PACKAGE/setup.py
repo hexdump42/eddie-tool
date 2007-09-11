9c5dee0fb12361a932e93edbda2a30ab732a0166
chris <chris@00129812-9848-11de-bf47-b33d4a03a5a8>
2007-09-11 16:22:10 +0000

    Restructed directories in setuptools package format, with pre-generated setup.py.

---
+++ eddie/branches/BRANCH_PYTHON_PACKAGE/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.36-svn'
+
+setup(name='Eddie-Tool',
+      version=version,
+      description="System monitoring agent.",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Chris Miles',
+      author_email='miles.chris@gmail.com',
+      url='http://eddie-tool.net/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
